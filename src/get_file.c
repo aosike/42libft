c131f263a1a0d9e3dafc2f09068d74cf4759957b
Aaron GUNDRY <agundry@e1z2r7p44.42.us.org>
2018-01-06 14:20:25 -0800

    changes?

--- src/get_file.c
+++ src/get_file.c
@@ -8,8 +8,7 @@ int		get_file(int fd, char **file)
 	ft_buf_init(&buf, BUFF_SIZE);
 	while (get_next_line(fd, &line))
 		ft_buf_add(&buf, line, ft_strlen(line)); //
-	if (*file = buf.buf) //strdup
+	if (ft_strdup(buf.buf, file) && free(buf)) //strdup & clean buf
 		return (1);
-	//clean buf?
 	return (-1);
 }
